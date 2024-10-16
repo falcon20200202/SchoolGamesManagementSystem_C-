feac604a4a8d92301fb273ca352057896f26e4dd
falcon20200202 <1104232532@qq.com>
2024-10-16 13:52:29 +0800

    [v2.0]

--- SGMS.cpp
+++ SGMS.cpp
@@ -2,227 +2,328 @@
 #include<string>
 #include<stdlib.h>
 
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/exception.h>
+#include "jdbc/mysql_driver.h"
+
 using namespace std;
 
-int menu();
-void Sum();
-void sort();
-void csh(); //1
-void xyskzl(); //2
-void gxyxxzf(); //3
-void gxyzfmccx(); //4
-void gxyzfcx(); //5
-void gxmhjydyxxxxg(); //6
-void Add(); //6
-int convert(int i); //6
+// 全局变量
+sql::Statement* stmt; // 全局变量声明
 
-struct Athlete {
-    int num;  //序号
-    string project;  //项目
-    int ranking;  //名次
-    int score;  //分数
-};
-
-struct collage {
-    int num;
-    string name;  //学院名称
-    Athlete Ma[10];  //男运动员
-    Athlete Wo[10];  //女运动员
-    int sum;
-    int sumM;
-    int sumW;
-};
-
-
-collage Class[6] = {
-    {1,"dianzigongcheng", {{1,"1100",1,7},{3,"跳远",1,7}}, {{2,"1100",1,7},{4,"跳远",1,7}}},
-    {2,"chuanbo", {{5,"1100",3,3},{7,"跳远",2,5}}, {{6,"1100",3,3},{8,"跳远",2,5}}},
-    {3,"shengmingkexue", {{9,"1100",2,5},{11,"跳远",5,1}}, {{10,"1100",2,5},{12,"跳远",5,1}}},
-    {4,"jidiangongcheng", {{13,"1100",4,2},{15,"跳远",4,2}}, {{14,"1100",4,2},{16,"跳远",4,2}}},
-    {5,"zhonge", {{17,"1100",6,0},{19,"跳远",3,3}}, {{18,"1100",6,0},{20,"跳远",3,3}}},
-    {6,"shuxue", {{21,"1100",5,1},{23,"跳远",6,0}}, {{22,"1100",5,1},{24,"跳远",6,0}}}
-};
-
-int N;  //参赛院系总数
-int M;  //参赛男子总数
-int W;  //参赛女子总数
-int JM = 2;  //每个学院的男运动员
-int JW = 2;  //每个学院的女运动员
-
-int main() {
-    int n = 1;
-
-    while (n > 0 && n < 7) {
-        n = menu();
-        switch (n) {
-        case 1: csh(); break;
-        case 2: xyskzl(); break;
-        case 3: gxyxxzf(); break;
-        case 4: gxyzfmccx(); break;
-        case 5: gxyzfcx(); break;
-        case 6: gxmhjydyxxxxg(); break;
-        }
-    }
-
-    return 0;
-}
 
 //////////////////////菜单//////////////////
 int menu() {
     int n;
     system("cls"); //清屏
-    cout << "运动会管理系统\n";
-    cout << "----------------\n";
-    cout << "1.初始化\n";
-    cout << "2.学院赛况总览\n";
-    cout << "3.各学院详细总分\n";
-    cout << "4.各学院总分名次查询\n";
-    cout << "5.各学院总分查询\n";
-    cout << "6.各项目获奖运动员信息修改\n";
-    cout << "7.结束程序\n";
+    cout << "运动会管理系统" << endl;
+    cout << "----------------" << endl;
+    cout << "1.【各学院-各项】分数总览" << endl;
+    cout << "2.【各学院-男女】总分总览" << endl;
+    cout << "3.学院总分名次查询" << endl;
+    cout << "4.【各项目】运动员排名查询" << endl;
+    cout << "5.运动员信息录入" << endl;
+    cout << "6.项目成绩录入" << endl;
+    cout << "7.结束程序" << endl;
     cout << "请输入所需功能前的序号:";
     cin >> n;
 
     while (n < 0 || n > 7) {
-        cout << "请重新输入所需功能前的序号，eg:1";
+        cout << "请重新输入所需功能前的序号，eg:1"<<endl;
         cin >> n;
     }
 
     return n;
 }
 
-//////////////////初始化1////////////////
-void csh() {
-    cout << "请输入参赛院系总数：";
-    cin >> N;
-    cout << "请输入参赛男子总数：";
-    cin >> M;
-    cout << "请输入参赛女子总数：";
-    cin >> W;
-}
+//1.【各学院-各项】分数总览
+void scoreAcademy(sql::Statement* stmt) {
+    system("cls"); //清屏
+    cout << "【各学院-各项】分数总览" << endl;
+    cout << "----------------" << endl;
+    string output_message;
 
-//////////////学院赛况总览2/////////////////
-void xyskzl() {
-    for (int i = 0; i < 6; i++) {
-        cout << Class[i].name << endl;
-        for (int j = 0; j < JM; j++) {
-            cout << "\t" << Class[i].Ma[j].project << "\n\t" << Class[i].Ma[j].num << "\t" << Class[i].Ma[j].score << "\n\t"
-                << Class[i].Wo[j].num << "\t" << Class[i].Wo[j].score << endl;
-        }
+    output_message = "SELECT a.academy_name,e.event_name, SUM(r.score) AS total_score FROM registration r JOIN students s ON r.student_id = s.student_id JOIN academy a ON r.academy_id JOIN events e ON r.event_id = e.event_id GROUP BY a.academy_name, e.event_name ORDER BY a.academy_name, e.event_name";
+    sql::ResultSet* res = stmt->executeQuery(output_message);
+    cout << "学院" << "\t项目" << "\t总分" << endl;
+    while (res->next()) {
+        cout << res->getString("academy_name")
+            << "\t" << res->getString("event_name")
+            << "\t" << res->getInt("total_score") << endl;
     }
-    cout << "返回请按1\n";
+    delete res; // 清理资源
+
+
     int temp;
-    cin >> temp;
-}
-
-//////////////////各学院总分查询3//////////////////////
-void gxyxxzf() {
-    int c[6], d[6], e[6];
-    for (int i = 0; i < 6; i++) {
-        c[i] = Class[i].Ma[0].score + Class[i].Ma[1].score;
-        d[i] = Class[i].Wo[0].score + Class[i].Wo[1].score;
-        e[i] = c[i] + d[i];
-    }
-    for (int i = 0; i < 6; i++) {
-        cout << "学院\t总分\t\t男子总分 女子总分\n";
-        cout << Class[i].name << "\t" << e[i] << "\t" << c[i] << "\t" << d[i] << endl;
-    }
-    cout << "返回请输入1\n";
-    int temp;
-    cin >> temp;
-}
-
-////////////////各学院总分名次查询4///////////
-void gxyzfmccx() {
-    int temp, c[6], d[6], e[6];
-    string a[6] = { "shuxue", "zhonge", "jidiangongcheng", "chuanbo", "shengmingkexue", "dianzigongcheng" };
-
-    for (int i = 0; i < 6; i++) {
-        c[i] = Class[i].Ma[0].score + Class[i].Ma[1].score;
-        d[i] = Class[i].Wo[0].score + Class[i].Wo[1].score;
-        e[i] = c[i] + d[i];
-    }
-
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6 - i - 1; j++) {
-            if (e[j] > e[j + 1]) {
-                temp = e[j]; e[j] = e[j + 1]; e[j + 1] = temp;
-            }
-        }
-    }
-
-    cout << "学院\t总分\n";
-    for (int i = 5; i >= 0; i--) {
-        cout << a[i] << "\t" << e[i] << endl;
-    }
     cout << "返回请输入1\n";
     cin >> temp;
 }
 
-/////////////////各学院总分查询5/////////////
-void gxyzfcx() {
-    int c[6], d[6], e[6];
-    for (int i = 0; i < 6; i++) {
-        c[i] = Class[i].Ma[0].score + Class[i].Ma[1].score;
-        d[i] = Class[i].Wo[0].score + Class[i].Wo[1].score;
-        e[i] = c[i] + d[i];
+//2.【各学院-男女】总分总览
+void scoreGender(sql::Statement* stmt) {
+    system("cls"); //清屏
+    cout << "【各学院-男女】总分总览" << endl;
+    cout << "----------------" << endl;
+    string output_message;
+    // 总分
+    output_message = "SELECT a.academy_name, SUM(r.score) AS total_score, SUM(CASE WHEN s.gender = 'male' THEN r.score ELSE 0 END) AS total_male_score, SUM(CASE WHEN s.gender = 'female' THEN r.score ELSE 0 END) AS total_female_score FROM registration r JOIN students s ON r.student_id = s.student_id JOIN academy a ON r.academy_id = a.academy_id GROUP BY a.academy_name";
+    sql::ResultSet* res = stmt->executeQuery(output_message);
+
+    cout << "学院" << "\t总分" << "\t男子总分" << "\t女子总分" << endl;
+    while (res->next()) {
+        cout << res->getString("academy_name")
+            << "\t " << res->getInt("total_score")
+            << "\t " << res->getInt("total_male_score")
+            << "\t\t " << res->getInt("total_female_score") << endl;
     }
 
+    delete res; // 清理资源
+
+
+    int temp;
+    cout << "返回请输入1\n";
+    cin >> temp;
+}
+
+//3.学院总分名次查询
+void rankAcademy(sql::Statement* stmt) {
+    system("cls"); //清屏
+    cout << "学院总分名次查询" << endl;
+    cout << "----------------" << endl;
+    string output_message;
+
+    output_message = "SELECT a.academy_name, SUM(r.score) AS total_score FROM registration r JOIN academy a ON r.academy_id = a.academy_id GROUP BY a.academy_name ORDER BY total_score DESC";
+    sql::ResultSet* res = stmt->executeQuery(output_message);
+    cout << "学院" << "\t总分" << endl;
+    while (res->next()) {
+        cout << res->getString("academy_name")
+            << "\t" << res->getInt("total_score") << endl;
+    }
+    delete res; // 清理资源
+
+
+
+    int temp;
+    cout << "返回请输入1\n";
+    cin >> temp;
+}
+
+//4.【各项目】运动员排名查询
+void rankAthlete(sql::Statement* stmt) {
+    system("cls"); //清屏
+    cout << "【各项目】运动员排名查询" << endl;
+    cout << "----------------" << endl;
+    cout << "请输入录入项目序号" << endl;
+    cout << "1.100m 2.200m 3.跳远 4.跳高" << endl;
+    string event_id;
+    cin >> event_id;
+    string output_message = "SELECT s.student_name, r.score FROM registration r JOIN students s ON r.student_id = s.student_id WHERE r.event_id = "+ event_id +" ORDER BY score DESC";
+    sql::ResultSet* res = stmt->executeQuery(output_message);
+    cout << "姓名\t分数" << endl;
+    while (res->next()) {
+        cout << res->getString("student_name")
+            << "\t" << res->getInt("score") << endl;
+    }
+
+    int temp;
+    cout << "返回请输入1\n";
+    cin >> temp;
+}
+
+
+//5.运动员信息录入
+void enterGender(sql::Statement* stmt) {
     int n;
-    cout << "1.电子工程\n2.传播\n3.生命科学\n4.机电工程\n5.中俄\n6.数学\n7.返回\n";
-    cout << "请输入序号\n";
+    system("cls"); //清屏
+    cout << "运动员信息录入"<<endl;
+    cout << "----------------" << endl;
+    cout << "1.添加运动员" << endl;
+    cout << "2.删除运动员" << endl;
+    cout << "3.修改运动员信息" << endl;
+    cout << "4.查询所有运动员报名信息" << endl;
+    cout << "请输入所需功能前的序号:";
     cin >> n;
+
+    while (n < 0 || n > 4) {
+        cout << "请重新输入所需功能前的序号，eg:1" << endl;
+        cin >> n;
+    }
+
+    // 1.添加运动员
+    if (n == 1) {
+        cout << "添加运动员" << endl;
+        string student_name;
+        string gender;
+        string academy_id;
+        string event_id;
+        cout << "请输入姓名：";
+        cin >> student_name;
+        cout << "请输入性别（male/female）：";
+        cin >> gender;
+        cout << "请输入学院id" << endl;
+        cout << "1.电子工程 2.传播 3.通信工程 4.机电工程 5.中俄 6.数学" << endl;
+        cin >> academy_id;
+        cout << "请输入填报项目序号" << endl;
+        cout << "1.100m 2.200m 3.跳远 4.跳高" << endl;
+        cin >> event_id;
+        
+        // 向students表添加
+        string add_message2students;
+        add_message2students = "insert into students(student_name, gender, academy_id) values('" + student_name + "', '" + gender + "',' " + academy_id + "')";
+
+        stmt->execute(add_message2students);
+        sql::ResultSet* res = stmt->executeQuery("SELECT LAST_INSERT_ID() AS id");
+        res->next();
+        int student_id = res->getInt("id");
+        // 向registration表添加
+        string add_message2registration;
+        add_message2registration = "insert into registration(student_id, academy_id, event_id, score) values('" + to_string(student_id) + "', '" + academy_id + "', '" + event_id + "', '0')";
+        stmt->execute(add_message2registration);
+
+        cout << "添加成功！你的序号为：";
+        cout << student_id << endl;
+        delete res;
+    }
+
+    // 2.删除运动员
+    else if (n == 2) {
+        cout << "删除运动员" << endl;
+        cout << "请输入运动员序号" << endl;
+        string num_athlete;
+        cin >> num_athlete;
+        string del_message;
+
+        try {
+            del_message = "DELETE FROM registration WHERE student_id = " + num_athlete;
+            stmt->execute(del_message);
+            del_message = "DELETE FROM students WHERE student_id = " + num_athlete;
+            stmt->execute(del_message);
+
+            cout << "删除成功" << endl;
+        }
+        catch (sql::SQLException& e) {
+            std::cerr << "SQL Error code: " << e.getErrorCode() << std::endl;
+            std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+            std::cerr << "Error message: " << e.what() << std::endl;
+        }
+
+    }
+    // 3.修改运动员报名信息
+    else if (n == 3) {
+        cout << "修改运动员报名信息" << endl;
+        cout << "请输入运动员序号" << endl;
+        string student_id;
+        cin >> student_id;
+        cout << "请输入要修改为的项目序号" << endl;
+        cout << "1.100m 2.200m 3.跳远 4.跳高" << endl;
+        string event_id;
+        cin >> event_id;
+        string update_message;
+        update_message = "UPDATE registration SET event_id = " + event_id + " WHERE student_id = " + student_id;
+
+        stmt->execute(update_message);
+        cout << "修改成功！" << endl;
+
+    }
+    // 4.查询所有运动员报名信息
+    else {
+        cout << "查询所有运动员报名信息" << endl;
+        string select_message;
+        try {
+            select_message = "SELECT registration.student_id, students.student_name, students.gender, events.event_name FROM registration JOIN students ON registration.student_id = students.student_id JOIN events ON registration.event_id = events.event_id";
+            cout << "序号\t姓名\t性别\t项目" << endl;
+            sql::ResultSet* res = stmt->executeQuery(select_message);
+            while (res->next()) {
+                cout << res->getString("student_id") << "\t" << res->getString("student_name")
+                    << " \t" << res->getString("gender") << "\t" << res->getString("event_name") << endl;
+            }
+            delete res;
+        }
+        catch (sql::SQLException& e) {
+            std::cerr << "SQL Error code: " << e.getErrorCode() << std::endl;
+            std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+            std::cerr << "Error message: " << e.what() << std::endl;
+        }
+    }   
+
+    int temp;
+    cout << "返回请输入1\n";
+    cin >> temp;
+}
+
+//6.项目成绩录入
+void enterScore(sql::Statement* stmt) {
+    string event_id;
+    system("cls"); //清屏
+    cout << "项目成绩录入" << endl;
+    cout << "----------------" << endl;
+    cout << "请输入录入项目序号" << endl;
+    cout << "1.100m 2.200m 3.跳远 4.跳高" << endl;
+    cin >> event_id;
+    string output_message;
+    output_message = "SELECT registration.student_id FROM registration WHERE event_id = "+ event_id;
+    cout << "报名该项目的学生为：" << endl;
+    try {
+        sql::ResultSet* res = stmt->executeQuery(output_message);
+        while (res->next()) {
+            cout << res->getString("student_id") << endl;
+        }
+        delete res;
+    }
+    catch (sql::SQLException& e) {
+        std::cerr << "SQL Error code: " << e.getErrorCode() << std::endl;
+        std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+        std::cerr << "Error message: " << e.what() << std::endl;
+    }
+
+    cout << "请输入对应得分" << endl;
+    sql::ResultSet* res = stmt->executeQuery(output_message);
+    while (res->next()) {
+        string score;
+        string student_id = res->getString("student_id");
+        cout << student_id << ": ";
+        cin >> score;
+        string input_message;
+        input_message = "UPDATE registration SET score = " + score + " WHERE student_id = " + student_id;
+        stmt->execute(input_message);
+    }
+    delete res;
+ 
+    cout << "录入成功！" << endl;
+
+    int temp;
+    cout << "返回请输入1\n";
+    cin >> temp;
+}
+
+int main() {
+//int __main(){
+    // 注册 MySQL 驱动程序
+    sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+    sql::Connection* con = driver->connect("tcp://公网ip:3306", "root", "密码");
+    // 设置字符集
+    con->setClientOption("character_set_results", "utf8mb4");
+
+    sql::Statement* stmt = con->createStatement();
+    stmt->execute("USE codertest"); // 切换到 codertest 数据库
+
+    int n = 1;
 
     while (n > 0 && n < 7) {
-        cout << Class[n - 1].name << "\t" << e[n - 1] << endl;
-        cout << "1.电子工程\n2.传播\n3.生命科学\n4.机电工程\n5.中俄\n6.数学\n7.返回\n";
-        cout << "请输入序号\n";
-        cin >> n;
-    }
-}
-
-///////////各项目获奖运动员信息修改6///////////////////
-void gxmhjydyxxxxg() {
-    int n;
-    cout << "1.增添\n2.删除\n3.返回\n";
-    cout << "请输入所需功能前的序号：\n";
-    cin >> n;
-
-    while (n < 1 || n > 3) {
-        cout << "请重新输入所需功能前的序号，eg:1";
-        cin >> n;
-    }
-
-    if (n == 1) {
-        Add();
-    }
-}
-
-void Add() {
-    int i, flag = 1;
-    char c;
-
-    while (flag == 1) {
-        cout << "请输入需要修改信息的学院序号：";
-        cin >> i;
-
-        cout << "请依次输入需要添加的运动员序号、比赛项目、比赛名次、比赛成绩：\n";
-        Athlete newAthlete;
-        cin >> newAthlete.num >> newAthlete.project >> newAthlete.ranking >> newAthlete.score;
-
-        cout << "男或女（M or W）:";
-        cin >> c;
-
-        if (c == 'M') {
-            Class[i - 1].Ma[JM] = newAthlete;
-            JM++;
+        n = menu();
+        switch (n) {
+        case 1: scoreAcademy(stmt); break;
+        case 2: scoreGender(stmt); break;
+        case 3: rankAcademy(stmt); break;
+        case 4: rankAthlete(stmt); break;
+        case 5: enterGender(stmt); break;
+        case 6: enterScore(stmt); break;
         }
-        else if (c == 'W') {
-            Class[i - 1].Wo[JW] = newAthlete;
-            JW++;
-        }
-
-        cout << "返回请按1";
-        cin >> flag;
     }
+    delete stmt;
+    delete con;
+    
+
+	return 0;
 }
---
+++ newTables.cpp
@@ -0,0 +1,103 @@
+﻿#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/exception.h>
+#include "jdbc/mysql_driver.h"
+//#include <windows.h>
+#include <iostream>
+
+//int main() {
+int __main() {
+    //SetConsoleOutputCP(CP_UTF8);
+
+    try {
+        // 注册 MySQL 驱动程序
+        sql::mysql::MySQL_Driver* driver = sql::mysql::get_mysql_driver_instance();
+        sql::Connection* con = driver->connect("tcp://公网ip:3306", "root", "密码");
+        // 设置字符集
+        con->setClientOption("character_set_results", "utf8mb4");
+
+        sql::Statement* stmt = con->createStatement();
+        stmt->execute("USE codertest"); // 切换到 codertest 数据库
+
+  
+        //stmt->execute("DROP TABLE IF EXISTS registration"); //删除表
+        //stmt->execute("DROP TABLE IF EXISTS students"); //删除表
+        //stmt->execute("DROP TABLE IF EXISTS academy"); //删除表
+        //stmt->execute("DROP TABLE IF EXISTS events"); //删除表
+     
+        //// 查看数据库的默认字符集
+        //std::unique_ptr<sql::ResultSet> res(stmt->executeQuery("SELECT DEFAULT_CHARACTER_SET_NAME FROM information_schema.SCHEMATA WHERE SCHEMA_NAME = 'codertest'"));
+        //while (res->next()) {
+        //    std::cout << "Default character set: " << res->getString(1) << std::endl;
+        //}
+
+        //// 查看表的状态
+        //res.reset(stmt->executeQuery("SHOW TABLE STATUS WHERE Name = 'events'"));
+        //while (res->next()) {
+        //    std::cout << "Table: " << res->getString("Name") << ", Character Set: " << res->getString("Collation") << std::endl;
+        //}
+        
+ //--------------------------------------------------------------------------------------------------------------------------------------//
+         添加运动项目表
+        stmt->execute("create table if not exists events (event_id int primary key auto_increment," // 运动项目id，主键，自动递增
+            "event_name varchar(100) not null)"); // 运动项目名称
+
+        // 添加学院表
+        stmt->execute("create table if not exists academy (academy_id int primary key auto_increment," // 学院id，主键，自动递增
+            "academy_name varchar(100) not null)"); // 学院名称
+
+        // 添加学生表
+        stmt->execute("create table if not exists students (student_id int primary key auto_increment," // 序号，主键，自动递增
+            "student_name varchar(255) not null," // 学生姓名
+            "gender enum('male','female') not null," // 性别
+            "academy_id int," // 学院id，外键
+            "constraint student_academy foreign key (academy_id) references academy(academy_id))");
+
+        // 添加报名表
+        stmt->execute("create table if not exists registration (student_id int," // 学生id，外键
+            "academy_id int," // 学院id，外键
+            "event_id int," // 运动项目id，外键
+            "score int," // 该学生的该项目分数
+            "constraint fk_student foreign key(student_id) references students(student_id),"
+            "constraint fk_academy foreign key(academy_id) references academy(academy_id),"
+            "constraint fk_event foreign key(event_id) references events(event_id))");
+
+        // 添加运动项目
+        stmt->execute("insert into events (event_name) values ('100m')");
+        stmt->execute("insert into events (event_name) values ('200m')");
+        stmt->execute("insert into events (event_name) values ('tiaoyuan')");
+        stmt->execute("insert into events (event_name) values ('tiaogao')");
+
+        // 添加学院
+        stmt->execute("insert into academy (academy_name) values ('dianzi')");
+        stmt->execute("insert into academy (academy_name) values ('chuanbo')");
+        stmt->execute("insert into academy (academy_name) values ('tongxin')");
+        stmt->execute("insert into academy (academy_name) values ('jidian')");
+        stmt->execute("insert into academy (academy_name) values ('zhonge')");
+        stmt->execute("insert into academy (academy_name) values ('shuxue')");
+
+//--------------------------------------------------------------------------------------------------------------------------------------//
+// 
+// 
+        // 添加学生
+        //stmt->execute("insert into students (student_name, gender, academy_id) values ('zhao', 'male', 1)");
+        //stmt->execute("insert into students (student_name, gender, academy_id) values ('qian', 'female', 1)");
+        //stmt->execute("insert into students (student_name, gender, academy_id) values ('sun', 'male', 1)");
+        //stmt->execute("insert into students (student_name, gender, academy_id) values ('li', 'female', 1)");
+   
+        // 添加报名
+        //stmt->execute("insert into registration (student_id, event_id) values (1, 1)"); // john doe 报名100米
+        //stmt->execute("insert into registration (student_id, event_id) values (2, 2)"); // jane smith 报名200米
+
+        // 清理资源
+        delete stmt;
+        delete con;
+    }
+    catch (sql::SQLException& e) {
+        std::cerr << "SQL Error code: " << e.getErrorCode() << std::endl;
+        std::cerr << "SQL state: " << e.getSQLState() << std::endl;
+        std::cerr << "Error message: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
